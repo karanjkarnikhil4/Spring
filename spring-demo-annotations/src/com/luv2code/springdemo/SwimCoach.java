191319cd455afed90c7ccd98ce1c0697759ffc9f
Nikhil <karanjkarnikhil4@gmail.com>
2019-09-28 20:42:33 +0530

    Injecting values from the properties file

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,32 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService fortuneService)
 	{
--- spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -10,12 +10,18 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		//get the bean from the spring container
 		//Coach coach =context.getBean("thatSillyCoach",Coach.class); this is for named bean id
-		Coach coach = context.getBean("swimCoach",Coach.class);
+		SwimCoach coach = context.getBean("swimCoach",SwimCoach.class);
 		//call a method on the bean
 		System.out.println(coach.getDailyWorkout());
 		
 		// call the method to get the fortune
 		System.out.println(coach.getDailyFortune());
+		
+		// call the method to get the email
+		System.out.println(coach.getEmail());	
+				
+		// call the method to get the team
+		System.out.println(coach.getTeam());	
 		
 		//close the context
 		context.close();
