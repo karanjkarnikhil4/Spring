39ff38668173146e86581dcd8a6e2e21cbe64917
Nikhil <karanjkarnikhil4@gmail.com>
2019-10-02 09:04:23 +0530

    Saving a student object using hibernate

---
+++ hibernate-demo/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,51 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+
+		//create the session factory
+		SessionFactory factory = new Configuration().
+				configure("hibernate.cfg.xml").
+				addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		
+		
+		//create session
+		Session session =factory.getCurrentSession();
+		
+		try
+		{
+			//create a student object
+			System.out.println("creating a new student object");
+			Student tempStudent = new Student("paul","wall","paul@luv2code.com");
+			
+			//start a transaction
+			session.beginTransaction();
+			
+			
+			//save the student object
+			session.save(tempStudent);
+			
+			//commit  the transaction
+			session.getTransaction().commit();
+			
+			
+			System.out.println("Done!");
+		}
+		
+		finally {
+			
+		}
+		
+		
+	}
+
+}
