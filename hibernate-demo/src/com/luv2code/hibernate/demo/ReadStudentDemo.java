9e4b4666ae4dc9f7e4cb972246e3e8ce20a22f0d
Nikhil <karanjkarnikhil4@gmail.com>
2019-10-02 12:33:36 +0530

    Retrieving object from the database

--- hibernate-demo/src/com/luv2code/hibernate/demo/ReadStudentDemo.java
+++ hibernate-demo/src/com/luv2code/hibernate/demo/ReadStudentDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class ReadStudentDemo {
 
 	public static void main(String[] args) {
 
@@ -23,16 +23,35 @@ public class CreateStudentDemo {
 		{
 			//create a student object
 			System.out.println("creating a new student object");
-			Student tempStudent = new Student("paul","wall","paul@luv2code.com");
+			Student tempStudent = new Student("Daffy","Duck","daffy@luv2code.com");
 			
 			//start a transaction
 			session.beginTransaction();
 			
 			
 			//save the student object
+			System.out.println(tempStudent.toString());
 			session.save(tempStudent);
+			System.out.println("Saving the student...");
 			
 			//commit  the transaction
+			session.getTransaction().commit();
+			
+			
+			//find out the student's ID:primary key
+			System.out.println("Saved student. Generated id: " +tempStudent.getId());
+			
+			//now get a new session and start the transaction
+			session =factory.getCurrentSession();
+			session.beginTransaction();
+			
+			
+			//retrieve student based on the id:primary key
+			System.out.println("\n Getting student with id: "+tempStudent.getId());
+			Student student =session.get(Student.class, tempStudent.getId());
+			System.out.println("Get Complete: "+student);
+			
+			//commit the transaction
 			session.getTransaction().commit();
 			
 			
